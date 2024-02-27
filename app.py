3020b5559c63e69f08c529d21d98f50da5fa05b7
Visharg Shah <vishargshah@gmail.com>
2024-02-27 06:50:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 import imports
- 
+import vertexai
+from vertexai.generative_models import GenerationConfig, GenerativeModel, Image, Part
+
+PROJECT_ID = "skilled-box-414804"
+LOCATION = "us-central1"
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
+model = GenerativeModel("gemini-1.0-pro")
 # Allow user to upload a Python file
 uploaded_file = st.file_uploader("Upload a Python file")
 file_content = ""
@@ -21,4 +28,20 @@ if uploaded_file is not None:
     # Optionally, display the code content
     st.code(file_content, language="python")
     print(file_content)
+
+
+prompt = """write test cases in .json for below python code """ + """
+ 
+""" + file_content
+
+testcases = model.generate_content(prompt, stream=True)
+
+
+for testcase in testcases:
+    with open('test_cases.text', 'a') as f:
+        f.write(testcase.text)  
+    
+st.write(testcase.text)    
+
+   
  
